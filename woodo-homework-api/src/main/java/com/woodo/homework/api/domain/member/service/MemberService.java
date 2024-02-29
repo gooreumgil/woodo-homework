3c51087c8777056df01475c76ea286e898dbf550
gooreumgil <gooreumgil47@gmail.com>
2024-02-29 19:56:55 +0900

    update  - member not_exists exception 추가

--- woodo-homework-api/src/main/java/com/woodo/homework/api/domain/member/service/MemberService.java
+++ woodo-homework-api/src/main/java/com/woodo/homework/api/domain/member/service/MemberService.java
@@ -1,11 +1,14 @@
 package com.woodo.homework.api.domain.member.service;
 
 import com.woodo.homework.api.domain.consignedbook.dto.ConsignedBookSaveRequest;
+import com.woodo.homework.api.exception.HttpException;
+import com.woodo.homework.api.exception.HttpExceptionCode;
 import com.woodo.homework.core.domain.consignedbook.ConsignedBook;
 import com.woodo.homework.core.domain.consignedbook.repository.ConsignedBookRepository;
 import com.woodo.homework.core.domain.member.Member;
 import com.woodo.homework.core.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +28,18 @@ public class MemberService {
 
     @Transactional
     public ConsignedBook addConsignedBook(Long id, ConsignedBookSaveRequest consignedBookSaveRequest) {
-        Member member = findById(id).orElseThrow(() -> new RuntimeException(""));
-        ConsignedBook consignedBook = ConsignedBook.create(consignedBookSaveRequest.getName(), consignedBookSaveRequest.getIsbn(), consignedBookSaveRequest.getRentalPrice());
+        Member member = findById(id)
+                .orElseThrow(() ->
+                        new HttpException(
+                                HttpStatus.BAD_REQUEST,
+                                HttpExceptionCode.NOT_EXISTS,
+                                "존재하지 않는 member 입니다."));
+
+        ConsignedBook consignedBook = ConsignedBook.create(
+                consignedBookSaveRequest.getName(),
+                consignedBookSaveRequest.getIsbn(),
+                consignedBookSaveRequest.getRentalPrice());
+
         member.addConsignedBook(consignedBook);
         return consignedBookRepository.save(consignedBook);
     }
